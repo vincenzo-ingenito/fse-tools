8f573f7506f0529e81245b36abc122c751dec96a
Vincenzo Ingenito <vincenzo.ingenito@hotmail.it>
2023-07-27 23:12:18 +0200

    Updates

--- src/routes/jwtGenerator/action_id.js
+++
@@ -1,6 +0,0 @@
-export const action_id_enum = [
-    { value: 'CREATE', text: 'CREATE' },
-    { value: 'UPDATE', text: 'UPDATE' },
-    { value: 'DELETE', text: 'DELETE' },
-    { value: 'REPLACE', text: 'REPLACE' }
-];
