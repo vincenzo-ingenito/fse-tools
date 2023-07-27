daaea66b88aafb82fb7afcd6d8f839e951306701
Vincenzo Ingenito <vincenzo.ingenito@hotmail.it>
2023-07-27 21:35:51 +0200

    feat: Update

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from "@sveltejs/adapter-static";
+import sveltePreprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	},
-	preprocess: vitePreprocess()
+  kit: {
+    adapter: adapter(),
+  },
+  preprocess: sveltePreprocess({
+    postcss: true,
+  }),
 };
 
 export default config;
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import adapter from "@sveltejs/adapter-static";
+import { sveltekit } from "@sveltejs/kit/vite";
+import sveltePreprocess from "svelte-preprocess";
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+  sveltekit: {
+    adapter: adapter(),
+  },
+  plugins: [sveltekit()],
+  preprocess: sveltePreprocess({
+    postcss: true,
+  }),
 };
 
 export default config;
