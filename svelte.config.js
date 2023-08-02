3823fb657ef69e0c91e19333b64335aa696a6be1
Vincenzo Ingenito <vincenzo.ingenito@hotmail.it>
2023-08-02 21:00:39 +0200

    feat: Update cfg

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ const config = {
   kit: {
     adapter: adapter(),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/gtw-tools' : '',
+      base: process.env.NODE_ENV === 'production' ? '/fse-tools' : '',
   }
   },
   preprocess: sveltePreprocess({
