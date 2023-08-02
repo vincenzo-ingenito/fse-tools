baa99396ffefb03e81d14e84ba3ffb87359b6b77
vincenzo-ingenito <vincenzo-ingenito@users.noreply.github.com>
2023-08-02 18:50:43 +0000

    deploy: 3ed5fa630bd540736d51e7bf91a14ccbed66df05

---
+++ _app/immutable/chunks/paths.6118b84e.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1l19fcq)==null?void 0:s.base)??"/gtw-tools";var t;const e=((t=globalThis.__sveltekit_1l19fcq)==null?void 0:t.assets)??a;export{e as a,a as b};
