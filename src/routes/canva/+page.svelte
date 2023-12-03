60f6a56d5e328661061d09844a305d6a4833fc6c
Vincenzo Ingenito <54367888+vincenzo-ingenito@users.noreply.github.com>
2023-12-03 23:16:36 +0100

    Update +page.svelte

--- src/routes/canva/+page.svelte
+++ src/routes/canva/+page.svelte
@@ -1,9 +1,12 @@
-<script> 
+<script>
   import { onMount, onDestroy } from 'svelte';
   import { writable } from 'svelte/store';
-  import Konva from 'konva';
+  import Konva from 'konva'; 
 
-
+  let showDialog = false;
+  let hexagonName = '';
+  let groupId = '';
+  let artifact = '';
   let stage;
   let layer;
   let hexagons = [];
@@ -27,9 +30,39 @@
   });
 
   function createHexagon() {
-    const hexagonName = prompt('Inserisci il nome per l\'esagono:');
-    if (!hexagonName) {
-      alert('Il nome non può essere vuoto. Riprova.');
+    // Apri la dialog con tre textarea
+    Dialog.show({
+      title: 'Inserisci i dettagli dell\'esagono',
+      content: `
+        <div>
+          <label for="hexagonName">Nome:</label>
+          <textarea bind:value={hexagonName}></textarea>
+        </div>
+        <div>
+          <label for="groupId">GroupId:</label>
+          <textarea bind:value={groupId}></textarea>
+        </div>
+        <div>
+          <label for="artifact">Artifact:</label>
+          <textarea bind:value={artifact}></textarea>
+        </div>
+      `,
+      buttons: [
+        {
+          text: 'Crea Esagono',
+          onClick: handleCreateHexagon,
+        },
+        {
+          text: 'Annulla',
+          onClick: handleDialogClose,
+        },
+      ],
+    });
+  }
+
+  function handleCreateHexagon() {
+    if (!hexagonName || !groupId || !artifact) {
+      alert('Nessun campo può essere vuoto. Riprova.');
       return;
     }
 
@@ -41,6 +74,8 @@
       fill: getRandomColor(),
       draggable: true,
       name: hexagonName,
+      groupId: groupId,
+      artifact: artifact,
     });
 
     layer.add(hexagon);
@@ -48,11 +83,25 @@
 
     // Aggiorna l'oggetto arrowObject
     arrowObject.update(obj => {
-      obj[hexagonName] = { id: hexagonName, link: [] };
+      obj[hexagonName] = { id: hexagonName, link: [], groupId: groupId, artifact: artifact };
       return obj;
     });
 
+    closeDialog();
+
+     // Resetta i campi della dialog
+     hexagonName = '';
+    groupId = '';
+    artifact = '';
     layer.draw();
+  }
+
+  function openDialog() {
+    showDialog = true;
+  }
+
+  function closeDialog() {
+    showDialog = false;
   }
 
   function connectHexagons() {
@@ -102,6 +151,27 @@
     return color;
   }
 
+  function handleSendToBackend() {
+    const objectsToSend = Object.values($arrowObject);
+
+    // Esegui la richiesta POST al tuo backend con gli oggetti creati
+    fetch('http://localhost:8080/v1', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(objectsToSend),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Risposta dal backend:', data);
+      // Aggiungi qui la logica per gestire la risposta del backend, se necessario
+    })
+    .catch(error => {
+      console.error('Errore durante la richiesta al backend:', error);
+    });
+  }
+
   // Pulisci l'abbonamento quando il componente viene distrutto
   onDestroy(() => {
     unsubscribe();
@@ -112,8 +182,46 @@
   #container {
     border: 1px solid #ccc;
   }
+
+  .overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 999;
+  }
+
+  .dialog {
+    background: #fff;
+    padding: 20px;
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  }
 </style>
 
 <div id="container"></div>
-<button on:click={createHexagon}>Crea Esagono</button>
+<button on:click={openDialog}>Crea Esagono</button>
+
+{#if showDialog}
+  <div class="overlay" on:click={closeDialog}>
+    <div class="dialog" on:click={e => e.stopPropagation()}>
+      <label for="hexagonName">Nome:</label>
+      <textarea bind:value={hexagonName}></textarea>
+
+      <label for="groupId">GroupId:</label>
+      <textarea bind:value={groupId}></textarea>
+
+      <label for="artifact">Artifact:</label>
+      <textarea bind:value={artifact}></textarea>
+
+      <button on:click={handleCreateHexagon}>Crea Esagono</button>
+    </div>
+  </div>
+{/if}
 <button on:click={connectHexagons}>Collega Esagoni</button>
+<button on:click={handleSendToBackend}>Invia al Backend</button>
